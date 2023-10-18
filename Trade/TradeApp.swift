e21fb167b20da11bd5b3439e4aa4dabfb6251b08
aleksey <a.baranov@vsa.su>
2023-10-18 13:48:58 +0500

    Initial Commit

---
+++ Trade/TradeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradeApp.swift
+//  Trade
+//
+//  Created by Алексей Баранов on 18.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct TradeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
